ed26f6d0c32ad14e624ad3a1cd21f558b8ac377f
Laura Kogler and Nicole Aptekar <pair+laura+nicole@funtimes.gov>
2015-08-24 23:03:08 -0700

    worlds most robust portfolio site can now take markdown

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ An artist-portfolio static site generator
 
 # how to do
 `bundle install`
+`bundle exec middleman server`
--- config.rb
+++ config.rb
@@ -27,25 +27,28 @@
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+data.works.keys.each do |name|
+  proxy "/#{name}.html", "/work.html", :locals => { :work => data.works[name].merge({:key => name}) }
+end
 
 ###
 # Helpers
 ###
 
 # Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def markdown(text)
+    Tilt['markdown'].new {text}.render
+  end
+end
 
 set :css_dir, 'stylesheets'
 
